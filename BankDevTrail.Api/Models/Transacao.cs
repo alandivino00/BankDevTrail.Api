6d892d31466356ed5a5a55895481fc36d7d82e98
alandivino00 <38597412+alandivino00@users.noreply.github.com>
2025-11-17 11:11:38 -0300

    entidades e pricipais relacionamentos

--- BankDevTrail.Api/Models/Transacao.cs
+++ BankDevTrail.Api/Models/Transacao.cs
@@ -2,7 +2,17 @@
 {
     public class Transacao
     {
-       
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Tipo Tipo { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public DateTime? DataHora { get; set; }
+
+        public Guid? ContaOrigemId { get; set; }
+
+        public Guid? ContaDestinoId { get; set; }
 
     }
 }
